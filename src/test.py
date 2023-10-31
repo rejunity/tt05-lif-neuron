b99939d478a84b37481efd006154a60f9982ba3c
rej <rzioma@acm.org>
2023-10-31 20:36:07 +0100

    Thorough test - covers all permutations of 5 bits inputs and 5 bits weights

--- src/test.py
+++ src/test.py
@@ -104,12 +104,91 @@ async def test_neuron_long(dut):
 
     await done(dut)
 
+@cocotb.test()
+async def test_neuron_loop(dut):
+
+    for w in range(4):
+        for x in range(1,5):
+            await reset(dut)
+            u = 0
+            spike_train = []
+
+            dut._log.info(f"load weights {bin(w)}")
+            dut.uio_in.value = 1
+            for v in struct.Struct('>I').pack(w):
+                dut.ui_in.value = v
+                await ClockCycles(dut.clk, 1)
+            print_chip_state(dut)
+
+            dut._log.info(f"set input {bin(x)}")
+            dut.uio_in.value = 0
+            for v in struct.Struct('>I').pack(x):
+                dut.ui_in.value = v
+                await ClockCycles(dut.clk, 1)
+            print_chip_state(dut)
+
+            dut._log.info("calculate")
+            dut.uio_in.value = 2
+            for i in range(16):
+                await ClockCycles(dut.clk, 1)
+                # print_chip_state(dut)
+                spike, u = neuron(x, w, last_u=u)
+                dut.uo_out[0] == spike
+                spike_train.append(dut.uo_out[0].value)
+
+            dut._log.info(f"OK {sum(spike_train)} {str(spike_train).replace(', ', '')}")
+
+
+    await done(dut)
+
+@cocotb.test()
+async def test_neuron_long_loop(dut):
+    await reset(dut)
+
+    input_range = (0, 64)
+    for w in range(64):
+        dut._log.info(f"weights: {bin(w)}, inputs: {input_range}")
+        spike_counts = []
+        for x in range(*input_range):
+
+            dut.rst_n.value = 0
+            await ClockCycles(dut.clk, 10)
+            dut.rst_n.value = 1
+
+            u = 0
+            spike_train = []
+
+            dut.uio_in.value = 1
+            for v in struct.Struct('>I').pack(w):
+                dut.ui_in.value = v
+                await ClockCycles(dut.clk, 1)
+
+            dut.uio_in.value = 0
+            for v in struct.Struct('>I').pack(x):
+                dut.ui_in.value = v
+                await ClockCycles(dut.clk, 1)
+
+            dut.uio_in.value = 2
+            for i in range(16):
+                await ClockCycles(dut.clk, 1)
+                spike, u = neuron(x, w, last_u=u)
+                dut.uo_out[0] == spike
+                spike_train.append(dut.uo_out[0].value)
+
+            spike_counts.append(sum(spike_train))
+        dut._log.info(spike_counts)
+
+
+    await done(dut)
+
 ### UTILS #####################################################################
 
 def print_chip_state(dut):
     try:
         internal = dut.tt_um_rejunity_lif_uut
-        print(  dut.ui_in.value, '|',
+        print(  "W" if dut.uio_in.value & 1 else "I",
+                "X" if dut.uio_in.value & 2 else " ",
+                dut.ui_in.value, '|',
                 internal.x.value, '*',
                 internal.w.value, '=',
                 int(internal.neuron.u_out), '|',
