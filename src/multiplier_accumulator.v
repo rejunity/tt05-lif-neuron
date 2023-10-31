a6728fc05828a6713c83c507918ccdbc62e32db9
rej <rzioma@acm.org>
2023-10-31 18:30:04 +0100

    Removed multiplier_stage

--- src/multiplier_accumulator.v
+++ src/multiplier_accumulator.v
@@ -7,13 +7,19 @@ module mulplier_accumulator #(parameter n_stage = 6) (
 
     wire [(2*(2**n_stage))-1:0] mult_out;
 
-    multiplier_stage #(n_stage) mult_uut (
-        .w(w),
-        .x(x),
-        .mult_out(mult_out)
-    );
+    // Generate instances of multiplier for each element in w and x
+    genvar i;
+    generate
+        for (i=0; i<(2**n_stage); i=i+1) begin : mult_i
+            multiplier multiplier (
+                .xi(x[i]),
+                .wi(w[i]),
+                .y(mult_out[2*i+1:2*i])
+            );
+        end
+    endgenerate
 
-    adder_tree #(n_stage) adder_uut (
+    adder_tree #(n_stage) adder (
         .wx(mult_out),
         .y_out(y_out)
     );
