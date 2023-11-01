c4ee7e3265a4c6eab26bd189e8ea3e02d95eb9f4
rej <rzioma@acm.org>
2023-11-01 11:38:31 +0100

    Simplifying file structure and reducing code scatter: multiplier stage

--- src/multiplier_accumulator.v
+++ src/multiplier_accumulator.v
@@ -1,8 +1,37 @@
+module multiplier (
+    input xi,
+    input wi,
+    output signed [1:0] y
+);
+    // An attempt to explain multiplier logic:
+    // x * w 
+    //
+    // x:0 ->  0
+    // x:1 ->  1
+    // w:0      -> -1
+    // w:1      ->  1
+    //
+    // 0 * 0 = 0 * -1 =  0 = 00
+    // 0 * 1 = 0 *  1 =  0 = 00
+    // 1 * 0 = 1 * -1 = -1 = 11
+    // 1 * 1 = 1 *  1 =  1 = 01
+
+    //       _ 
+    // xw    wx   y10 
+    // 00 => 10 => 00
+    // 01 => 00 => 00
+    // 10 => 11 => 11
+    // 11 => 01 => 01
+
+    assign y[0] = xi;
+    assign y[1] = xi & (~wi);
+endmodule
+
 
 module mulplier_accumulator #(parameter n_stage = 6) (
     input [(2**n_stage)-1:0] w,
     input [(2**n_stage)-1:0] x,
-    output [(n_stage+1):0] y_out
+    output signed [(n_stage+1):0] y_out
 );
 
     wire [(2*(2**n_stage))-1:0] mult_out;
@@ -25,3 +54,4 @@ module mulplier_accumulator #(parameter n_stage = 6) (
     );
 
 endmodule
+
