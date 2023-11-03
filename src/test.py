9443d69ecd2e466fd6fe49912c0d3bd972880643
rej <rzioma@acm.org>
2023-11-03 20:53:00 +0100

    Don't set bn parameters from test.py

--- src/test.py
+++ src/test.py
@@ -354,10 +354,12 @@ async def setup_params(dut, shift=0, threshold=5, bias=0, bn_scale=BN_SCALE, bn_
         bn_scale = 0b1010
     elif bn_scale == 4.5:
         bn_scale = 0b1101
+    else
+        bn_scale = 0b0100 # default to 1
     await setup_control(dut, SETUP_THRESHOLD, threshold)
     await setup_control(dut, SETUP_BIAS, bias)
     await setup_control(dut, SETUP_SHIFT, shift)
-    await setup_control(dut, SETUP_BN_PARAMS, ((bn_add & 0xf) << 4) + bn_scale)
+    # await setup_control(dut, SETUP_BN_PARAMS, ((bn_add & 0xf) << 4) + bn_scale)
 
 async def execute(dut, clk=1):
     dut.uio_in.value = EXECUTE
